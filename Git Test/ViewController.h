b487501d7b7db03f8562c3d6dc6f3f14620ffff8
Amy Holms <amy@formfireglassworks.com>
2014-10-12 17:51:11 -0700

    first commit

---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Amy Holms on 10/12/14.
+//  Copyright (c) 2014 FormFire Develops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Amy Holms on 10/12/14.
+//  Copyright (c) 2014 FormFire Develops. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
