568a4fc3411179dc768353000a34d0a0c4ff790a
Amy Holms <amy@formfireglassworks.com>
2014-11-09 15:20:55 -0800

    added test class

---
+++ FFDTest.h
@@ -0,0 +1,13 @@
+//
+//  FFDTest.h
+//  Git Test
+//
+//  Created by Amy Holms on 11/9/14.
+//  Copyright (c) 2014 FormFire Develops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFDTest : NSObject
+
+@end
---
+++ FFDTest.m
@@ -0,0 +1,13 @@
+//
+//  FFDTest.m
+//  Git Test
+//
+//  Created by Amy Holms on 11/9/14.
+//  Copyright (c) 2014 FormFire Develops. All rights reserved.
+//
+
+#import "FFDTest.h"
+
+@implementation FFDTest
+
+@end
